e800e3fff6ed83c26befa69de78c39f758dcac38
Arthur <git@arthur.pro>
2020-11-22 20:23:27 -0500

    Updated Kurento to v6.15.0

--- 6.15.0/scripts/build-deps.sh
+++ 6.15.0/scripts/build-deps.sh
@@ -6,69 +6,69 @@ chown -R root:root /root/rpmbuild
 
 cd /root/rpmbuild/SPECS
 
-# for spec in `ls *.spec`; do
-#     rpmsrc $spec
-# done
-#
-# rpmbb opencore-amr.spec
-# yumi ../RPMS/x86_64/opencore-amr-0.1.3-4.el7.x86_64.rpm ../RPMS/x86_64/opencore-amr-devel-0.1.3-4.el7.x86_64.rpm
-#
-# rpmbb vo-aacenc.spec vo-amrwbenc.spec
-# yumi ../RPMS/x86_64/vo-aacenc-0.1.2-3.el7.x86_64.rpm ../RPMS/x86_64/vo-aacenc-devel-0.1.2-3.el7.x86_64.rpm ../RPMS/x86_64/vo-amrwbenc-0.1.3-2.el7.x86_64.rpm ../RPMS/x86_64/vo-amrwbenc-devel-0.1.3-2.el7.x86_64.rpm
-#
-# yumbd x265.spec
-# rpmbb x265.spec
-# yumi ../RPMS/x86_64/x265-libs-2.9-3.el7.x86_64.rpm ../RPMS/x86_64/x265-devel-2.9-3.el7.x86_64.rpm
-#
-# rpmbb xvidcore.spec
-# yumi ../RPMS/x86_64/xvidcore-1.3.4-2.el7.x86_64.rpm ../RPMS/x86_64/xvidcore-devel-1.3.4-2.el7.x86_64.rpm
-#
-# rpmbb fdk-aac.spec
-# yumi ../RPMS/x86_64/fdk-aac-0.1.5-0.1.gita0bd8aa.el7.x86_64.rpm ../RPMS/x86_64/fdk-aac-devel-0.1.5-0.1.gita0bd8aa.el7.x86_64.rpm
-#
-# yumbd faad2.spec
-# rpmbb faad2.spec
-# yumi ../RPMS/x86_64/faad2-libs-2.7-9.el7.x86_64.rpm ../RPMS/x86_64/faad2-devel-2.7-9.el7.x86_64.rpm
-#
-# yumbd x264.spec --define '_with_bootstrap 1'
-# rpmbb x264.spec --define '_with_bootstrap 1'
-# yumi ../RPMS/x86_64/x264-libs-0.148-23.20170521gitaaa9aa8_bootstrap.el7.x86_64.rpm ../RPMS/x86_64/x264-devel-0.148-23.20170521gitaaa9aa8_bootstrap.el7.x86_64.rpm
-#
-# yumbd ffmpeg.spec
-# rpmbb ffmpeg.spec
-# yumi ../RPMS/x86_64/ffmpeg-libs-3.4.8-1.el7.x86_64.rpm ../RPMS/x86_64/libavdevice-3.4.8-1.el7.x86_64.rpm ../RPMS/x86_64/ffmpeg-devel-3.4.8-1.el7.x86_64.rpm
-#
-# yumbd gpac.spec
-# rpmbb gpac.spec
-#
-# yumi ../RPMS/x86_64/gpac-libs-0.7.1-8.el7.x86_64.rpm ../RPMS/x86_64/gpac-devel-0.7.1-8.el7.x86_64.rpm
-#
-# rpmbb x264.spec
-# yumi ../RPMS/x86_64/x264-libs-0.148-23.20170521gitaaa9aa8.el7.x86_64.rpm ../RPMS/x86_64/x264-devel-0.148-23.20170521gitaaa9aa8.el7.x86_64.rpm
-#
-# yumbd libde265.spec
-# rpmbb libde265.spec
-# yumi ../RPMS/x86_64/libde265-1.0.2-6.el7.x86_64.rpm ../RPMS/x86_64/libde265-devel-1.0.2-6.el7.x86_64.rpm
-#
-# yumbd automake.spec
-# rpmbb automake.spec
-# yumi ../RPMS/noarch/automake-1.15-9.el7.noarch.rpm
-#
-# yumbd opus.spec
-# rpmbb opus.spec
-# yumi ../RPMS/x86_64/opus-1.1.3-1.el7.x86_64.rpm ../RPMS/x86_64/opus-devel-1.1.3-1.el7.x86_64.rpm
-#
-# yumbd rtmpdump.spec
-# rpmbb rtmpdump.spec
-# yumi ../RPMS/x86_64/librtmp-2.4-7.20160224.gitfa8646d.el7.x86_64.rpm ../RPMS/x86_64/librtmp-devel-2.4-7.20160224.gitfa8646d.el7.x86_64.rpm
-#
-# yumbd openh264.spec
-# rpmbb openh264.spec
-# yumi ../RPMS/x86_64/openh264-1.5.0-4.el7.x86_64.rpm ../RPMS/x86_64/openh264-devel-1.5.0-4.el7.x86_64.rpm
-#
-# yumbd kms-openjpeg2.spec
-# rpmbb kms-openjpeg2.spec
-# yumi ../RPMS/x86_64/kms-openjpeg2-2.1.0-7.el7.x86_64.rpm ../RPMS/x86_64/kms-openjpeg2-devel-2.1.0-7.el7.x86_64.rpm
+for spec in `ls *.spec`; do
+    rpmsrc $spec
+done
+
+rpmbb opencore-amr.spec
+yumi ../RPMS/x86_64/opencore-amr-0.1.3-4.el7.x86_64.rpm ../RPMS/x86_64/opencore-amr-devel-0.1.3-4.el7.x86_64.rpm
+
+rpmbb vo-aacenc.spec vo-amrwbenc.spec
+yumi ../RPMS/x86_64/vo-aacenc-0.1.2-3.el7.x86_64.rpm ../RPMS/x86_64/vo-aacenc-devel-0.1.2-3.el7.x86_64.rpm ../RPMS/x86_64/vo-amrwbenc-0.1.3-2.el7.x86_64.rpm ../RPMS/x86_64/vo-amrwbenc-devel-0.1.3-2.el7.x86_64.rpm
+
+yumbd x265.spec
+rpmbb x265.spec
+yumi ../RPMS/x86_64/x265-libs-2.9-3.el7.x86_64.rpm ../RPMS/x86_64/x265-devel-2.9-3.el7.x86_64.rpm
+
+rpmbb xvidcore.spec
+yumi ../RPMS/x86_64/xvidcore-1.3.4-2.el7.x86_64.rpm ../RPMS/x86_64/xvidcore-devel-1.3.4-2.el7.x86_64.rpm
+
+rpmbb fdk-aac.spec
+yumi ../RPMS/x86_64/fdk-aac-0.1.5-0.1.gita0bd8aa.el7.x86_64.rpm ../RPMS/x86_64/fdk-aac-devel-0.1.5-0.1.gita0bd8aa.el7.x86_64.rpm
+
+yumbd faad2.spec
+rpmbb faad2.spec
+yumi ../RPMS/x86_64/faad2-libs-2.7-9.el7.x86_64.rpm ../RPMS/x86_64/faad2-devel-2.7-9.el7.x86_64.rpm
+
+yumbd x264.spec --define '_with_bootstrap 1'
+rpmbb x264.spec --define '_with_bootstrap 1'
+yumi ../RPMS/x86_64/x264-libs-0.148-23.20170521gitaaa9aa8_bootstrap.el7.x86_64.rpm ../RPMS/x86_64/x264-devel-0.148-23.20170521gitaaa9aa8_bootstrap.el7.x86_64.rpm
+
+yumbd ffmpeg.spec
+rpmbb ffmpeg.spec
+yumi ../RPMS/x86_64/ffmpeg-libs-3.4.8-1.el7.x86_64.rpm ../RPMS/x86_64/libavdevice-3.4.8-1.el7.x86_64.rpm ../RPMS/x86_64/ffmpeg-devel-3.4.8-1.el7.x86_64.rpm
+
+yumbd gpac.spec
+rpmbb gpac.spec
+
+yumi ../RPMS/x86_64/gpac-libs-0.7.1-8.el7.x86_64.rpm ../RPMS/x86_64/gpac-devel-0.7.1-8.el7.x86_64.rpm
+
+rpmbb x264.spec
+yumi ../RPMS/x86_64/x264-libs-0.148-23.20170521gitaaa9aa8.el7.x86_64.rpm ../RPMS/x86_64/x264-devel-0.148-23.20170521gitaaa9aa8.el7.x86_64.rpm
+
+yumbd libde265.spec
+rpmbb libde265.spec
+yumi ../RPMS/x86_64/libde265-1.0.2-6.el7.x86_64.rpm ../RPMS/x86_64/libde265-devel-1.0.2-6.el7.x86_64.rpm
+
+yumbd automake.spec
+rpmbb automake.spec
+yumi ../RPMS/noarch/automake-1.15-9.el7.noarch.rpm
+
+yumbd opus.spec
+rpmbb opus.spec
+yumi ../RPMS/x86_64/opus-1.1.3-1.el7.x86_64.rpm ../RPMS/x86_64/opus-devel-1.1.3-1.el7.x86_64.rpm
+
+yumbd rtmpdump.spec
+rpmbb rtmpdump.spec
+yumi ../RPMS/x86_64/librtmp-2.4-7.20160224.gitfa8646d.el7.x86_64.rpm ../RPMS/x86_64/librtmp-devel-2.4-7.20160224.gitfa8646d.el7.x86_64.rpm
+
+yumbd openh264.spec
+rpmbb openh264.spec
+yumi ../RPMS/x86_64/openh264-1.5.0-4.el7.x86_64.rpm ../RPMS/x86_64/openh264-devel-1.5.0-4.el7.x86_64.rpm
+
+yumbd kms-openjpeg2.spec
+rpmbb kms-openjpeg2.spec
+yumi ../RPMS/x86_64/kms-openjpeg2-2.1.0-7.el7.x86_64.rpm ../RPMS/x86_64/kms-openjpeg2-devel-2.1.0-7.el7.x86_64.rpm
 
 yumbd kms-boost.spec
 rpmbb kms-boost.spec --without python3
